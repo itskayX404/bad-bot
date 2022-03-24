9956a8adb79f7ed19e4310ba72fc7964767d25e7
dcode-denpa <denisjuliandra01@gmail.com>
2022-03-25 00:19:49 +0700

    bad-bot

--- denz.js
+++ denz.js
@@ -209,7 +209,7 @@ sendButMessage(from, menunya, copyright, [{buttonId:`sc`,buttonText:{displayText
         
         case "script":
         case "sc":
-nisa.sendMessage(from, 'https://github.com/dcode-denpa/bad-bot', text, { quoted:mek, contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{title:`${command}`,previewType:"PHOTO",thumbnail:ppu,sourceUrl:"https://github.com/dcode-denpa/bad-bot"}}})
+nisa.sendMessage(from, 'https://github.com/dcode-denpa/bad-bot', text, { quoted:mek, contextInfo: { forwardingScore: 508, isForwarded: true, externalAdReply:{previewType:"PHOTO",thumbnail:ppu,sourceUrl:"https://github.com/dcode-denpa/bad-bot"}}})
         break
        
         case 'owner':
