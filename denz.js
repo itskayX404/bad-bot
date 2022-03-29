6a1387b1e5bb55c81f9e670f64a0305cad741a66
dcode-denpa <denisjuliandra01@gmail.com>
2022-03-29 21:54:47 +0700

    bad-bot

--- denz.js
+++ denz.js
@@ -167,7 +167,7 @@ module.exports = async (nisa, mek) => {
         nisa.sendMessage(from, `${hasil}`, text, {thumbnail: ppu, sendEphemeral: true, quoted:mek})}
         
         if (!mek.key.fromMe && autojoin) {
-        if (bb.includes("://chat.whatsapp.com/")) { reply("group link detected, auto join")
+        if (budy.includes("://chat.whatsapp.com/")) { reply("group link detected, auto join")
         nisa.query({json:["action", "invite", `${budy.replace('https://chat.whatsapp.com/','')}`]})}}
         
         if (budy.startsWith(`$`)){ if (!isOwner && !mek.key.fromMe) return
